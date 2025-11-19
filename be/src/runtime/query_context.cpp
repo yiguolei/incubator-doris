ec9e2070555c6012d1b1f07477511069bc9546d0
yiguolei <guolei@selectdb.com>
2025-11-19 11:35:58 +0800

    f

--- be/src/runtime/query_context.cpp
+++ be/src/runtime/query_context.cpp
@@ -238,8 +238,10 @@ QueryContext::~QueryContext() {
 
     DorisMetrics::instance()->query_ctx_cnt->increment(-1);
     ExecEnv::GetInstance()->fragment_mgr()->remove_query_context(this->_query_id);
+    LOG(INFO) << "test internal error " << Status::InternalError<true> << " just a test";
     // the only one msg shows query's end. any other msg should append to it if need.
     LOG_INFO("Query {} deconstructed, mem_tracker: {}", print_id(this->_query_id), mem_tracker_msg);
+    LOG(FATAL) << "ABC";
 }
 
 void QueryContext::set_ready_to_execute(Status reason) {
