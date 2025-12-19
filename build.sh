c378f3f4643cc781ee7463058e433bb810fb3306
Calvin Kirs <acm_master@163.com>
2025-12-19 14:38:52 +0800

    branch-2.1: [chore]Upgrade dependencies and remove hudi-scanner (#6271)
    ## Proposed changes
    
    Issue Number: close #xxx
    
    <!--Describe your changes.-->

--- build.sh
+++ build.sh
@@ -477,7 +477,6 @@ if [[ "${BUILD_HIVE_UDF}" -eq 1 ]]; then
 fi
 if [[ "${BUILD_BE_JAVA_EXTENSIONS}" -eq 1 ]]; then
     modules+=("fe-common")
-    modules+=("be-java-extensions/hudi-scanner")
     modules+=("be-java-extensions/hadoop-hudi-scanner")
     modules+=("be-java-extensions/java-common")
     modules+=("be-java-extensions/java-udf")
@@ -725,7 +724,6 @@ EOF
 
     extensions_modules=("java-udf")
     extensions_modules+=("jdbc-scanner")
-    extensions_modules+=("hudi-scanner")
     extensions_modules+=("hadoop-hudi-scanner")
     extensions_modules+=("paimon-scanner")
     extensions_modules+=("max-compute-scanner")
